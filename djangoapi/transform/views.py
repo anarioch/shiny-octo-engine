24f78d29234cfc127522a5a44c5d1c04c159ac7b
Jerome Dolman <jerome.dolman@gmail.com>
2024-02-24 09:30:07 +1300

    feat: Added transform API to django server

--- djangoapi/transform/views.py
+++ djangoapi/transform/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+import requests
+import xml.etree.ElementTree as ET
+import json
+
+
+# Transform the data from the source XML format into a Python object
+def transform(xmlText):
+    xml = ET.fromstring(xmlText)
+    # TODO: Add error handling to validate input format
+    return {
+        "id": xml.find("id").text,
+        "name": xml.find("name").text,
+        "description": xml.find("description").text,
+    }
+
+def index(request):
+    id = '1'
+
+    # Fetch
+    r = requests.get(f'https://raw.githubusercontent.com/MiddlewareNewZealand/evaluation-instructions/main/xml-api/{id}.xml')
+    if r.status_code != 200:
+        return HttpResponse(status=502, content=f"Error accessing backend API, received status code '{r.status_code}'")
+
+    # Transform
+    data = transform(r.text)
+
+    # Output
+    output = json.dumps(data)
+    return HttpResponse(content_type="text/json", content=output)
