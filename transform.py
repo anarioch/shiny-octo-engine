ead139a7a3d6e20603e3f6383691d13cd29f01a0
Jerome Dolman <jerome.dolman@gmail.com>
2024-02-23 16:23:23 +1300

    refactor: Extracted data transformation to its own function

--- transform.py
+++ transform.py
@@ -4,21 +4,29 @@ import requests
 import xml.etree.ElementTree as ET
 import json
 
-def process(id):
-    r = requests.get(f'https://raw.githubusercontent.com/MiddlewareNewZealand/evaluation-instructions/main/xml-api/{id}.xml')
-    if r.status_code != 200:
-        print(f"Error: target API call failed with code '{r.status_code}'")
-        return
-    
-    xml = ET.fromstring(r.text)
-    data = {
+def transform(xmlText):
+    xml = ET.fromstring(xmlText)
+    # TODO: Add error handling to validate input format
+    return {
         "id": xml.find("id").text,
         "name": xml.find("name").text,
         "description": xml.find("description").text,
     }
 
+def process(id):
+    # Fetch
+    r = requests.get(f'https://raw.githubusercontent.com/MiddlewareNewZealand/evaluation-instructions/main/xml-api/{id}.xml')
+    if r.status_code != 200:
+        print(f"Error: target API call failed with code '{r.status_code}'")
+        return
+
+    # Transform
+    data = transform(r.text)
+
+    # Output
     output = json.dumps(data)
     print(output)
+
 
 if __name__ == "__main__":
     id = '1'
