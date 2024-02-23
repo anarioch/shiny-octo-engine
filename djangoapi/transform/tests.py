feafe113f0b2290a616d6af9d199b480c164ab9a
Jerome Dolman <jerome.dolman@gmail.com>
2024-02-24 10:50:36 +1300

    test: Added Django test for API

--- djangoapi/transform/tests.py
+++ djangoapi/transform/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+class CompaniesTestCase(TestCase):
+    def test_companies_valid(self):
+        client = Client()
+        response = client.get('/v1/companies/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.content, b'{"id": "1", "name": "MWNZ", "description": "..is awesome"}')
+
+    def test_companies_invalid(self):
+        client = Client()
+        response = client.get('/v1/companies/5')
+        self.assertEqual(response.status_code, 404)
